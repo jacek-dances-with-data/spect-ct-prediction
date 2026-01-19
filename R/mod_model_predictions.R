e1e5d8c64a26ab284a6dc867daa6e059fd2d0c03
Jacek <podlewski.jacek@gmail.com>
2026-01-19 20:08:30 +0100

    adjust panel height

--- R/mod_model_predictions.R
+++ R/mod_model_predictions.R
@@ -14,6 +14,7 @@ mod_model_predictions_ui <- function(id) {
   ns <- NS(id)
   tagList(
     tags$div(
+      style="height:95%",
       sidebarLayout(
         sidebarPanel(
           sliderInput(
